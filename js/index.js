c59a06f400f17e84bb690f8546e91172f21191cb
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-12-15 13:20:21 +0700

    add top design for dekstop not responseve

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+var options = {
+    strings: ['Ach. Faisol S. Arifin'],
+    stringsElement: null,
+    typeSpeed: 60,
+    startDelay: 1000,
+    backSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: "<i style='font-weight:normal;color:rgba(0,0,0,0.5);'>_</i>",
+    contentType: 'html'
+
+};
+
+var typed = new Typed('.typed', options);
