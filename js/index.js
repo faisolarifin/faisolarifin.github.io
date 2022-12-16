925e1944db7c5c50101ba92e2742259873870273
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-12-16 16:52:47 +0700

    add sertifikat and swipper

--- js/index.js
+++ js/index.js
@@ -15,3 +15,22 @@ var options = {
 };
 
 var typed = new Typed('.typed', options);
+
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    initialSlide: 2,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
