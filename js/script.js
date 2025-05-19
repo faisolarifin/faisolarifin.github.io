25f3e7e636d85761a520e39c3eb426a4c9b0a0f8
ach.arifin <ach.arifin@jatis.com>
2025-05-19 09:04:02 +0700

    feat: animated, responsive, portofolio page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var options = {
+    strings: ['Software Engineer', 'AI Developer'],
+    stringsElement: null,
+    typeSpeed: 60,
+    startDelay: 1000,
+    backSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: "<i style='font-weight:normal;color:rgba(0,0,0,0.5);'>_</i>",
+    contentType: 'html'
+
+};
+
+var typed = new Typed('.typed', options);
